1d997c90f9c74754bbe233c4a1a9acd2b0ddd5c9
Rick Ells <rickells@D-128-208-228-228.dhcp4.washington.edu>
2015-10-23 11:53:41 -0700

    Updates developed on staff/rells/wordpress toward design

---
+++ footer.php
@@ -0,0 +1,41 @@
+    <div role="contentinfo" class="uw-footer">
+            <p style="margin: 40px 0 40px 0;"><a href="http://uw.edu/uwit/">
+              <img src="http://staff.washington.edu/rells/wordpress/wp-content/uploads/2012/12/uwit_logo_la.png"></a></p>
+        <nav role="navigation" aria-label="IT Resources" style="margin-bottom: 0;">
+            <ul class="footer-links" style="margin-top: 10px;">
+              <li><a href="http://www.washington.edu/itconnect/myrequests/">MyRequests</a></li>
+              <li><a href="http://www.washington.edu/itconnect/servicestatus/" >Service Status</a></li>
+              <li><a href="http://www.washington.edu/itconnect/it-connect-home/tools/">IT Tools for You</a></li>
+              <li><a href="http://www.washington.edu/itconnect/help/" >Help</a></li>
+              <li><a href="mailto:help@uw.edu">help@uw.edu</a></li>
+              <li><a href="tel:+2062215000" >206-221-5000</a></li>
+            </ul>
+            <ul class="footer-links" style="margin-top: .5em">
+              <li><a href="http://www.washington.edu/maps/#!/uwtc">In Person: UW Tower, C3000, M-F 8-5</a></li>
+            </ul>
+            <ul class="footer-social" style="margin-bottom: 0;">
+              <li><a class="twitter" href="http://twitter.com/UWITNews">Twitter</a></li>
+              <li><a class="youtube" href="http://www.youtube.com/user/uwinfotech">YouTube</a></li>
+            </ul>
+       </nav>
+        <nav role="navigation" aria-label="footer links" style="margin-top: -20px; " >
+            <ul class="footer-links">
+              <li><a href="http://www.uw.edu/accessibility">Accessibility</a></li>
+              <li><a href="http://www.washington.edu/safety">Campus Safety</a></li>
+              <li><a href="http://myuw.washington.edu/">My UW</a></li>
+              <li><a href="http://www.washington.edu/online/privacy/">Privacy</a></li>
+              <li><a href="http://www.washington.edu/online/terms/">Terms</a></li>
+           </ul>
+       </nav>
+
+        <p>&copy; <?php echo date("Y"); ?> University of Washington  |  Seattle, WA</p>
+
+    </div>
+
+    </div><!-- #uw-container-inner -->
+    </div><!-- #uw-container -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
