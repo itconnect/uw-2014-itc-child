08fd9e320c0ed9f499e5f4b38a1be744ce762bc9
Rick Ells <rells@uw.edu>
2016-05-17 10:03:54 -0700

    [ITC-875] swapping the default search area with js

--- js/itconnect.js
+++ js/itconnect.js
@@ -1,16 +1,29 @@
 (function(){
-    // NAMESPACE THIS STUFF
+	ITConnect = {
+		search: {
+        	switchDefault: function(){
+        		// Swaps UW to Current Site as the default for searches
+        		$('#search-labels input[type="radio"]').each(function(){
+        			$this = $(this);
+					if ($this.val() == 'site') {
+						$this.parent().addClass('checked');
+						$this.closest('#search-labels').prepend($this.parent());
+						$this.prop('checked', true);
+						$this.trigger('click');
+					} else if ($this.val() == 'uw') {
+						$this.parent().removeClass('checked');
+		                $this.prop('checked', false);
+					}
+				});
+        	}
+		},
+		init: function(){
+			this.search.switchDefault();
+		}
+	}
+
 
 	$(window).load(function() {
-		$('#search-labels input[type="radio"]').each(function(){
-			if ($(this).val() == 'site') {
-				$(this).parent().addClass('checked');
-				$(this).closest('#search-labels').prepend($(this).parent());
-				$(this).prop('checked', true);
-			} else if ($(this).val() == 'uw') {
-				$(this).parent().removeClass('checked');
-                $(this).prop('checked', false);
-			}
-		});
+		ITConnect.init();
 	});
 })();
