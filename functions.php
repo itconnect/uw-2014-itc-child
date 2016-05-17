97d74a2e195571e2dabe537ae8222eff61b0e8a3
Rick Ells <rells@uw.edu>
2016-05-17 10:05:37 -0700

    [ITC-875] cleaning up functions.php to remove old enqueue

--- functions.php
+++ functions.php
@@ -1,14 +1,4 @@
 <?php
-/**
-* Enqueues child theme stylesheet, loading first the parent theme stylesheet.
-*
-*function themify_custom_enqueue_child_theme_styles() {
-*	wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
-*}
-*
-*add_action( 'wp_enqueue_scripts', 'themify_custom_enqueue_child_theme_styles' );
-*/
-
 /** 
 * Enqueus child theme stylesheet and javascript, loading first the parent theme stylesheet
 */
