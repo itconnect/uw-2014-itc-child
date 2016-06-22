e7fa5a4060f8ebf4eb15d9a4baf1085326a2393a
Nick Rohde <ncrohde@uw.edu>
2016-06-22 14:30:57 -0700

    [ITC-887] Search enhancments parent branch

--- functions.php
+++ functions.php
@@ -15,6 +15,7 @@ add_action( 'wp_enqueue_scripts', 'itc_child_enqueue' );
 */
 add_editor_style();
 
+
 /**
 * Adds classes to body tag for CSS specificity over parent theme
 */
@@ -26,11 +27,12 @@ function itconnect_body_classes($classes) {
     return $classes;
 }
 
+
 /**
 * Enhancements for the Relevanassi search plugin
 */
-add_filter('relevanssi_match', 'custom_field_weights');
 
+// Increases weight of featured and top level pages
 function custom_field_weights($match) {
 
 	$featured = get_post_meta($match->doc, 'featured', true);
@@ -46,5 +48,26 @@ function custom_field_weights($match) {
 	
 	return $match;
 }
+add_filter('relevanssi_match', 'custom_field_weights');
+
+
+/**
+* Adds support for tags to pages (in addition to posts)
+*   1) Registers taxonomies for page type
+*   2) Includes tags on archive pages, modifies query object to return pages for archives
+*/
+function add_taxonomies_to_pages() {
+	register_taxonomy_for_object_type('post_tag', 'page');
+}
+
+function tags_archives( $wp_query ) {
+	if ($wp_query->get('tag'))
+		$wp_query->set('post_type', 'any');
+} 
+
+add_action('init', 'add_taxonomies_to_pages');
+if (!is_admin()) {
+	add_action('pre_get_posts', array($this, 'tags_archives'));
+} 
 
 ?>
