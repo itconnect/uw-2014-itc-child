0ec2f65438860d2ccc914bb4a372a1e586c7315c
Platy Platypus <rells@uw.edu>
2016-11-30 16:22:40 -0800

    [ITC-966] sortable review table

--- inc/review_audit.php
+++ inc/review_audit.php
@@ -8,12 +8,14 @@
 */
 
 function reviewedOnAudit() {
+	echo '<script src="' . get_stylesheet_directory_uri() . '/js/jquery.tablesorter.min.js"></script>';
+	echo '<table id="reviewed" class="tablesorter"><thead><tr><th style="width:75%;">Page</th><th style="width:25%;">Last Reviewed</th></tr></thead><tbody>';
 	// args
 	$args = array(
 		'post_type' => 'page',
 		'posts_per_page'  => -1,
-		'meta_key'  => 'reviewed_on',
-		'orderby' => 'meta_value_num',
+		//'meta_key'  => 'reviewed_on',
+		//'orderby' => 'meta_value_num',
 		'order' => 'ASC'
 	);
 
@@ -24,12 +26,24 @@ function reviewedOnAudit() {
 	while($wp_query->have_posts()) {
 		$wp_query->the_post();
 
-		$date = date_create(get_field('reviewed_on'));
 
-		//echo get_post_meta($post->ID,’speaking_event_date’,true) . ‘<br />’;
+		if (get_field('reviewed_on')) {
+			$date = date_create(get_field('reviewed_on'));
+			//$date = date_format($date,'M j, Y');
+			$date = '<span class="review_date">' . date_format($date, 'm/d/Y') . '</span>';
+			/*
+			*  ALSO MAKE RED IF OLDER THAN 1 YEAR
+			*/
 
-		echo '<li><a href="' . get_the_permalink() . '">' . get_the_title() . '</a> – ' . date_format($date,'m/d/Y') . '</li>';
+
+		} else {
+			$date = '<span class="needs_review">Needs review</span>';
+		};
+
+		echo '<tr><td><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></td><td>' . $date . '</td></tr>';
 
 	}
+	echo '</tbody></table>';
+	echo '<script>$(document).ready(function(){$("#reviewed").tablesorter({sortList: [[1,0]]});}); </script>';
 }
 add_shortcode('reviewedOn', 'reviewedOnAudit');
