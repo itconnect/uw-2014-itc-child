beeab902633099e41c0a3d4997f158574cd0a20b
Nick Rohde <ncrohde@uw.edu>
2017-06-15 13:53:53 -0700

    adding SVG icons and styles

--- functions.php
+++ functions.php
@@ -35,6 +35,118 @@ function itconnect_body_classes($classes) {
 
 
 /**
+* Adds support for SVG via TinyMCE editor
+*/
+
+add_filter('tiny_mce_before_init', 'allow_svg_in_tinymce');
+
+function allow_svg_in_tinymce( $init ) {
+
+	// NOTE: This doesn't take into account any security concerns regarding SVG.
+	//       It doesn't remove potential vulnerable elements, etc. It just allows
+	//       SVG, as is, assuming that the uploader is trusted. However, it would
+	//       be easy to remove elements such as script if desired. Just remove from
+	//       the array.
+
+	$svgElemList = array(
+		'a',
+		'altGlyph',
+		'altGlyphDef',
+		'altGlyphItem',
+		'animate',
+		'animateColor',
+		'animateMotion',
+		'animateTransform',
+		'circle',
+		'clipPath',
+		'color-profile',
+		'cursor',
+		'defs',
+		'desc',
+		'ellipse',
+		'feBlend',
+		'feColorMatrix',
+		'feComponentTransfer',
+		'feComposite',
+		'feConvolveMatrix',
+		'feDiffuseLighting',
+		'feDisplacementMap',
+		'deDistantLight',
+		'feFlood',
+		'feFuncA',
+		'feFuncB',
+		'feFuncG',
+		'feFuncR',
+		'feGaussianBlur',
+		'feImage',
+		'feMerge',
+		'feMergeNode',
+		'feMorphology',
+		'feOffset',
+		'fePointLight',
+		'feSpecularLighting',
+		'feSpotLight',
+		'feTile',
+		'feTurbulance',
+		'filter',
+		'font',
+		'font-face',
+		'font-face-format',
+		'font-face-name',
+		'font-face-src',
+		'font-face-url',
+		'foreignObject',
+		'g',
+		'glyph',
+		'glyphRef',
+		'hkern',
+		'image',
+		'line',
+		'lineGradient',
+		'marker',
+		'mask',
+		'metadata',
+		'missing-glyph',
+		'pmath',
+		'path',
+		'pattern',
+		'polygon',
+		'polyline',
+		'radialGradient',
+		'rect',
+		'script',
+		'set',
+		'stop',
+		'style',
+		'svg',
+		'switch',
+		'symbol',
+		'text',
+		'textPath',
+		'title',
+		'tref',
+		'tspan',
+		'use',
+		'view',
+		'vkern'
+	);
+
+	// extended_valid_elements is the list of elements that TinyMCE allows. This checks
+	// to make sure it exists, and then implodes the SVG element list and adds it. The
+	// format of each element is 'element[attributes]'. The array is imploded, and turns
+	// into something like '...svg[*],path[*]...'
+	
+	if ( isset( $init['extended_valid_elements'] ) ) {
+		$init['extended_valid_elements'] .= ','.implode('[*],',$svgElemList).'[*]';
+	}
+
+	// return value
+	return $init;
+}
+
+
+
+/**
 * Enhancements for the Relevanassi search plugin
 */
 require_once('inc/search_enhancements.php');
