3200bd5fa4bc953d7332bc02c78ed58ffc248ba0
Nick Rohde <ncrohde@uw.edu>
2017-11-03 13:55:14 -0700

    [ITC-1055] filtering content types on search results page

--- search.php
+++ search.php
@@ -16,6 +16,19 @@
 
       <div id='main_content' class="uw-body-copy" tabindex="-1">
 
+        <form role="search" method="get" id="searchform" class="searchform" action="<?php echo set_url_scheme( home_url('/') ) ?>">
+          <div>
+            <label class="screen-reader-text" for="s">Search for:</label>
+            <input type="text" value="" name="s" id="s" placeholder="Search for:" autocomplete="off">
+            <input type="submit" id="searchsubmit" value="Search">
+            <input type="checkbox" id="pages" name="pages" value="pages">
+            <label for="pages">Documentation</label>
+            <input type="checkbox" id="news" name="news" value="news">
+            <label for="news">News and communications</label>
+            <input type="checkbox" id="services" name="services" value="services">
+            <label for="services">Service Catalog</label>
+          </div>
+        </form>
 
         <?php
           if ( have_posts() ) :
