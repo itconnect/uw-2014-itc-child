f3cbde4f52df5d748bf7f9602717eb4bf5dde318
Nick Rohde <ncrohde@uw.edu>
2017-11-03 14:03:57 -0700

    [ITC-1055] filtering content types on search results page

--- search.php
+++ search.php
@@ -21,12 +21,14 @@
             <label class="screen-reader-text" for="s">Search for:</label>
             <input type="text" value="" name="s" id="s" placeholder="Search IT Connect for:" autocomplete="off">
             <input type="submit" id="searchsubmit" value="Search">
-            <input type="checkbox" id="pages" name="pages" value="true">
-            <label for="pages">Documentation</label>
-            <input type="checkbox" id="news" name="news" value="true">
-            <label for="news">News and communications</label>
-            <input type="checkbox" id="services" name="services" value="true">
-            <label for="services">Service Catalog</label>
+            <div class="filters">
+              <input type="checkbox" id="pages" name="pages" value="true">
+              <label for="pages">Documentation</label>
+              <input type="checkbox" id="news" name="news" value="true">
+              <label for="news">News and communications</label>
+              <input type="checkbox" id="services" name="services" value="true">
+              <label for="services">Service Catalog</label>
+            </div>
           </div>
         </form>
 
