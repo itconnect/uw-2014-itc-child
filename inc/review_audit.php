dcfcd212e36398a332b3fc033045a2544079b1ed
Nick Rohde <ncrohde@uw.edu>
2017-12-07 16:43:23 -0800

    [ITC-1060] added table functionality and fileds

--- inc/review_audit.php
+++ inc/review_audit.php
@@ -52,7 +52,7 @@ function reviewedOnAudit() {
 		echo '<tr><td><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></td><td>' . $date . '</td><td>' . $reviewer . '</td><td>' . $contacts .'</td><td>' . $serviceoffering . '</td></tr>';
 	}
 	echo '</tbody></table>';
-	echo '<script>$(document).ready(function() { $("#reviewed").DataTable({"pageLength": 100, "processing":true});} );</script>';
+	echo '<script>$(document).ready(function() { $("#reviewed").DataTable({"pageLength": 2000, "processing":true});} );</script>';
 	return ob_get_clean();
 }
 add_shortcode('reviewedOn', 'reviewedOnAudit');
