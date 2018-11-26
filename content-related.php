e16d477956c1d96d4e6884f577773e1060329649
Nick Rohde <ncrohde@uw.edu>
2018-11-26 15:55:43 -0800

    conditional display or related content

--- content-related.php
+++ content-related.php
@@ -1,10 +1,17 @@
 <?php
 
-if (get_field('display_related_information')){
+if ( get_field('display_related_information') && (get_field('related_information') || get_field('service_catalog') || get_field('request_forms')) ){
 
 	$out .= '<div class="related-content">';
 
 	$out .= '<b>Related Information</b>';
+
+	$relatedinfo = get_field('related_information');
+	if ($relatedinfo) {
+		foreach $relatedinfo as $related {
+			$out .= 'link ';
+		}
+	}
 
 	$out .= '</div>';
 
