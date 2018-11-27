debd7bc1d9d4bb075698ce43a359d88d3ca0389d
Nick Rohde <ncrohde@uw.edu>
2018-11-27 09:51:38 -0800

    [ITC-1082] related info widget

--- content-related.php
+++ content-related.php
@@ -1,14 +1,41 @@
-<?php
+<?php if( have_rows('related_information') ): ?>
 
+	<ul class="slides">
+
+	<?php while( have_rows('related_information') ): the_row(); 
+
+		// vars
+		$link = get_sub_field('related_information_link');
+
+		?>
+
+		<li class="slide">
+
+			<?php if( $link ): ?>
+				<a href="<?php echo $link; ?>">Link</a>
+			<?php endif; ?>
+
+		</li>
+
+	<?php endwhile; ?>
+
+	</ul>
+
+<?php endif; ?>
+
+<?php
+/*
 if ( get_field('display_related_information') && (get_field('related_information') || get_field('service_catalog') || get_field('request_forms')) ){
 
 	$out .= '<div class="related-content">';
 
-	$out .= '<b>Related Information</b>';
 
 	$relatedinfo = get_field('related_information');
 	if ($relatedinfo) {
-		foreach $relatedinfo as $related {
+
+		$out .= '<b>Related Information</b>';
+		
+		foreach ($relatedinfo as $related) {
 			$out .= 'link ';
 		}
 	}
@@ -18,5 +45,5 @@ if ( get_field('display_related_information') && (get_field('related_information
 	echo $out;
 
 }
-
+*/
 ?>
