b351bf8318a8b3af997cb139fab13ddb940f1668
Nick Rohde <ncrohde@uw.edu>
2019-08-27 10:22:05 -0700

    ITC Child SCat launch prep

--- search.php
+++ search.php
@@ -47,10 +47,11 @@
               <input type="checkbox" id="svcnws" name="svcnws" value="true" checked>
               <label for="svcnws">Service News</label>
             </fieldset>
+            <div style="font-size: 14px;">UW-IT's full list of services are in the <a href="https://uw.service-now.com/sp?id=sc_home">UW-IT Service Catalog</a>.</div>
+
           </div>
         </form>
-        <div stle="font-size: 14px;">UW-IT's full list of services are in the <a href="https://uw.service-now.com/sp?id=sc_home">UW-IT Service Catalog</a>.</div>
-
+       
         <?php
           if ( have_posts() ) :
             while ( have_posts() ) : the_post();
