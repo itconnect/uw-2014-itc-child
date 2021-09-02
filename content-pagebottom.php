f32b2c01d2d878e6d9e15c8e2a0d1e242f1c6a07
Nick Rohde <nickrohde@Nicks-MacBook-Pro.local>
2021-09-02 11:27:21 -0700

    [ITC-t4] Updaing let us know link to point to new feedback page

--- content-pagebottom.php
+++ content-pagebottom.php
@@ -7,7 +7,7 @@ if (get_field('reviewed_on')) {
 	echo '<div class="itc-updated-date">Last reviewed ' . date_format(date_create(get_field('reviewed_on')), 'F j, Y') . '</div>'; 
 }
 the_tags('<div class="tags"><span>Tags: </span>','','</div>'); 
-echo '<div class="report-link-container">See a problem on this page? <a class="report-link" href="javascript:void(0);">Let us know</a>.<div id="report-form">';
+echo '<div class="report-link-container">See a problem on this page? <a class="report-link" href="/help/page-feedback/">Let us know</a>.<div id="report-form">';
 //echo do_shortcode('[contact-form-7 id="42260" title="Report a problem"]');
 echo '</div></div>';
 echo '</div>';
